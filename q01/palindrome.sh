e54e2b4cfeeca5480c3f39646a9d8a3eaf35dc1a
Sumeet Mahalingapur <sumeetam1@gmail.com>
2025-09-09 00:56:25 +0530

    Create palindrome.sh

---
+++ q01/palindrome.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Enter the string to be checked"
+read str
+length=${#str}
+reverse=""
+
+for (( i=length-1; i>=0; i-- ))
+do
+    reverse=$reverse${str:$i:1}
+done
+
+if [[ $reverse == "$str" ]]
+then
+    echo "The string $str is a palindrome"
+else
+    echo "The string $str is not a palindrome"
+fi
